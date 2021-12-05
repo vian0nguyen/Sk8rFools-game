85b17403e1f97c805c58b2e6cf8f106678247885
vian0nguyen <vian0nguyen@gmail.com>
2021-12-05 08:57:12 -0500

    got cycling to work!

--- Assets/Scripts/ChildrenCycle.cs
+++ Assets/Scripts/ChildrenCycle.cs
@@ -6,10 +6,16 @@ public class ChildrenCycle : MonoBehaviour
 {
     public int arrayCount;
     public int arrayLimit;
-    // Start is called before the first frame update
-    void Start()
+    public int i = 0;
+
+    public List<SpriteRenderer> sprites;
+
+    private void Start()
     {
+        RetrieveChildren();
         
+
+        sprites = new List<SpriteRenderer>();
     }
 
     // Update is called once per frame
@@ -17,15 +23,16 @@ public class ChildrenCycle : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            Debug.Log("down");
-            RetrieveChildren();
+            Debug.Log("down " + arrayCount);
+            ChildrenCycler();
 
             // Step to next position in array
             arrayCount++;
 
             // If Array count excedes Array index then go back to first position in array
-            if (arrayCount >= arrayLimit)
+            if (arrayCount > arrayLimit)
             {
+                Debug.Log("too many!");
                 arrayCount = 0;
             }
         }
@@ -33,18 +40,48 @@ public class ChildrenCycle : MonoBehaviour
 
     public void RetrieveChildren()
     {
-        Debug.Log(transform.childCount);
-        int i = 0;
+        //only number finder
+        //int i = 0; //i think initiallizing i inside the function is setting it to always equal 0
         //Array to hold all child obj
         GameObject[] allChildren = new GameObject[transform.childCount];
+        arrayLimit = transform.childCount;
 
         //Find all child obj and store to that array
         foreach (Transform child in transform)
         {
+            //just numbers
             allChildren[i] = child.gameObject;
-            i += 1;
             arrayCount = i;
+            
+
         }
-        Debug.Log(transform.childCount);
+        //the sprite finder
+        // SpriteRenderer sprites = GetComponentInChildren<SpriteRenderer>();
+        foreach (SpriteRenderer child in sprites)
+        {
+            //just sprite finder
+            var renderer = sprites[i].GetComponent<SpriteRenderer>();
+            
+            //Add it to the list if it does
+            if (renderer)
+            {
+                sprites.Add(renderer);
+            }
+        }
+        Debug.Log("I found this many " + transform.childCount);
+    }
+
+    public void ChildrenCycler()
+    {
+        foreach (SpriteRenderer child in sprites)//turn off not selected
+        {
+            child.enabled = false; //?
+        }
+
+        sprites[arrayCount].enabled = true; //turns on the current selected
+                                            //var renderer = sprites[i].GetComponent<SpriteRenderer>();
+                                            //  sprites[arrayCount].enabled = true;
+        
+        
     }
 }
