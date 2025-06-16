1037873ca65a45008a6dac872e4fbd45ffaa183a
vian0nguyen <42777634+vian0nguyen@users.noreply.github.com>
2025-06-16 14:47:38 -0400

    revert to unborked branch

---
+++ Assets/QuestManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Ink.Runtime;
+
+
+public class QuestManager : MonoBehaviour
+{
+    public InkHandler IH;
+    public int health;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        IH.story.variablesState["player_health"] = 100;
+
+        
+    }
+
+    private void Update()
+    {
+        //health = (int)IH.story.variablesState["player_health"];
+    }
+}
+
